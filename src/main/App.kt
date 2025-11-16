a4f00bcaa791f7a25a96b5a53a4692a34b511731
RYAN JAMES <realcoder2023@gmail.com>
2025-11-15 23:17:46 -0500

    added random math to source code

--- src/main/App.kt
+++ src/main/App.kt
@@ -4,6 +4,8 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.withContext
+import java.util.Random
+import kotlin.math.abs
 import kotlin.time.Duration.Companion.milliseconds
 
 suspend fun greet() = withContext(Dispatchers.Default) {
@@ -16,6 +18,36 @@ suspend fun greet() = withContext(Dispatchers.Default) {
 suspend fun greetUser() {
 
     greet().await()
+}
+
+interface Maths {
+    fun add(a: Int, b: Int): Int
+    fun sub(a: Int, b: Int): Int
+    fun mul(a: Int, b: Int): Int
+    fun div(a: Int, b: Int): Int
+}
+
+class Math : Maths {
+    override fun add(a: Int, b: Int): Int {
+        return a + b
+    }
+
+    override fun sub(a: Int, b: Int): Int {
+
+        return abs(a - b)
+    }
+
+
+    override fun mul(a: Int, b: Int): Int {
+
+        return abs(a * b)
+    }
+
+
+    override fun div(a: Int, b: Int): Int {
+
+        return abs(a / b)
+    }
 }
 
 interface Commands {
@@ -48,6 +80,8 @@ suspend fun main() {
     println("Enter One Command: ")
     val input: String = readln()
 
+    val isMathInput = input.contains("/Math")
+
     if (input.contains("/")) {
 
         listCommands.forEach { e ->
@@ -58,21 +92,97 @@ suspend fun main() {
     else if (input.contains("/Timer")) {
 
         var n = 1
-        while(n > 0) {
+        while (n > 0) {
 
             n += 1
 
             timeSleep().await()
 
-            if (n == 25 * 60) {
-                println("Done")
-                break
-            }
+            val isMoreGreater = n == 25 * 60
 
-            else {
+            if (isMoreGreater) {
+                println("Done")
+            } else {
                 println("Seconds: $n")
             }
+
         }
+
     }
 
+    else if (isMathInput){
+        val options: List<String> = listOf("+", "-", "*", "/")
+        val rand = Random()
+        val index = rand.nextInt(0, options.size)
+        val operator = options[index]
+
+        val addMath = Math()
+
+        val isPlus = operator == "+"
+        val isSub = operator == "-"
+        val isMul = operator == "*"
+        val isDiv = operator == "/"
+
+        val num1 = rand.nextInt(1, 10)
+        val num2 = rand.nextInt(1, 10)
+
+        if (isPlus) {
+            println("What is: $num1 + $num2")
+            val answer = readln().toInt()
+            if (answer == addMath.add(num1, num2)) {
+
+                println("Correct Answer")
+            }
+            else if (answer != addMath.add(num1, num2)) {
+
+                println("Incorrect Answer The Answer was: ${abs(addMath.add(num1, num2))}")
+            }
+
+        }
+
+        else if (isSub) {
+
+            println("What is: $num1 - $num2")
+            val answer = readln().toInt()
+            if (answer == addMath.sub(num1, num2)) {
+
+                println("Correct Answer")
+            }
+            else if (answer != addMath.sub(num1, num2)) {
+
+                println("Incorrect Answer The Answer was: ${abs(addMath.sub(num1, num2))}")
+            }
+
+        }
+
+        else if (isMul) {
+
+            println("What is: $num1 * $num2")
+            val answer = readln().toInt()
+            if (answer == addMath.sub(num1, num2)) {
+
+                println("Correct Answer")
+            }
+            else if (answer != addMath.sub(num1, num2)) {
+
+                println("Incorrect Answer The Answer was: ${abs(addMath.sub(num1, num2))}")
+            }
+
+        }
+
+        else if (isDiv) {
+
+            println("What is: $num1 / $num2")
+            val answer = readln().toInt()
+            if (answer == addMath.div(num1, num2)) {
+
+                println("Correct Answer")
+            }
+            else if (answer != addMath.div(num1, num2)) {
+
+                println("Incorrect Answer The Answer was: ${abs(addMath.div(num1, num2))}")
+            }
+
+        }
+    }
 }
